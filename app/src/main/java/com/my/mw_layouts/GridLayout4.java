b119eef7a669707d7056c041a577601217e3b318
dimon1ion <dmitry-ben10@mail.ru>
2023-05-07 16:23:34 +0400

    Initial commit

---
+++ app/src/main/java/com/my/mw_layouts/GridLayout4.java
@@ -0,0 +1,14 @@
+package com.my.mw_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridLayout4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout4);
+    }
+}
---
+++ app/src/main/java/com/my/mw_layouts/RelativeLayout2.java
@@ -0,0 +1,14 @@
+package com.my.mw_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayout2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout2);
+    }
+}
